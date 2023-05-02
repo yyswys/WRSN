d236db35f97de85ac83ac267d33201c3ae0e370e
UvvZ <60565043+yyswys@users.noreply.github.com>
2023-05-02 18:21:08 +0800

    Add files via upload

--- WSN/WSN/ServiceQueue.h
+++ WSN/WSN/ServiceQueue.h
@@ -1,6 +1,7 @@
 //
 // Created by 22306 on 2023/4/15.
 //
+#include"vector"
 #include "WRSN.h"
 #include "WCV.h"
 #ifndef WSN_SERVICEQUEUE_H
@@ -20,13 +21,17 @@ struct Request {
 };
 
 
-
+/*
+ * 时间优先级
+ */
 float time(WCV Car,N Node,int n)
 {
     return ((Node.e / Node.q - Car.te) * n) /
            (Car.tl - Car.te);
 }
-
+/*
+ * 空间优先级
+ */
 float distance(WCV Car,N Node,int n)
 {
     float d = sqrt((Car.x - Node.x) * (Car.x - Node.x) +
@@ -35,16 +40,21 @@ float distance(WCV Car,N Node,int n)
             (Car.df - Car.dn);
 }
 
+
 float score(float d,float t) {
     float a = 1;
     float b = 1;
     return a * t + b * d + log10(t * d + 1);
 }
 
+/*
+ * 节点1、节点2、充电车、节点个数
+ * 根据论文中的公式判定两个节点获得充电优先级
+ */
 bool op(N N1,N N2,WCV Car,int n) {
     float n1_car_distance = distance(Car, N1, n);
-    float n1_car_time = time(Car, N2, n);
-    float n2_car_distance = distance(Car, N1, n);
+    float n1_car_time = time(Car, N1, n);
+    float n2_car_distance = distance(Car, N2, n);
     float n2_car_time = time(Car, N2, n);
     float final_score_n1 = score(n1_car_distance, n1_car_time);
     float final_score_n2 = score(n2_car_distance, n2_car_time);
@@ -55,16 +65,25 @@ bool op(N N1,N N2,WCV Car,int n) {
         return false;
     }
 }
-
-bool checkCanWaitDeadLine(WCV Car,N node){
-    float d = sqrt((Car.x - node.x) * (Car.x - node.x) +
-                   (Car.y - node.y) * (Car.y - node.y));
-    if (node.e - (d / Car.v) * node.q > 0) {//充电设备到达前不会死亡
-        return true;
-    } else
+//传入车、要充电的节点，判断在小车到达给节点充电之前是否会死亡
+//bool checkCanWaitDeadLine(WCV Car,N n[],int ID) {
+/*
+ * 判断在小车到达前是否会死亡
+ * 不改变节点、小车的值
+ */
+bool checkCanWaitDeadline(WCV Car,N node){
+    float d = 0;
+    //N node = n[ID];
+    d = sqrt((Car.x - node.x) * (Car.x - node.x) +
+             (Car.y - node.y) * (Car.y - node.y));
+    if (node.e - ((d / Car.v) * node.q) < 0) {//充电设备到达前不会死亡
         return false;
+    } else
+        return true;
 }
-
+/*
+ * 虚拟充电，运动过程中小车没电、小车的电不够充满以及充完后无法回返补充则不去进行充电
+ */
 bool fakeCharge(WCV Car,N Node)
 {
     float d = sqrt((Car.x - Node.x) * (Car.x - Node.x) +
@@ -75,14 +94,20 @@ bool fakeCharge(WCV Car,N Node)
     //error in charge
     //error after charge
     if (Car.E - (d / Car.v) * Car.qm - //error in move
-        Car.qc * (900 - Node.e) / Node.qr - //error in charge
+        Car.qc * (Node.emax - Node.e) / Node.qr - //error in charge
         (d1 / Car.v) * Car.qm < 0) {    //error after charge
         return false;
     } else
         return true;
 }
 
-//真实充电，返回花费时间，将其他节点的状态也向后拨动这样多的时间
+/*
+ * 小车前往充电
+ * 改变参数有：小车的电量————路程耗电、补满节点耗电
+ * 节点电量————满状态
+ * 小车位置————更新为节点位置
+ * 函数返回参数中的t包含了运动过程和充电过程的总时间，便于更新节点状态
+ */
 void realCharge(WCV &Car,N &Node,float &t) {
     float distance = sqrt((Car.x - Node.x) * (Car.x - Node.x) +
                           (Car.y - Node.y) * (Car.y - Node.y));
@@ -92,7 +117,7 @@ void realCharge(WCV &Car,N &Node,float &t) {
     float tm = distance / Car.v;
 
     float qc = Car.qc;
-    float tc = (900 - Node.e) / Node.qr;
+    float tc = (Node.emax - Node.e) / Node.qr;
 
 //    float Ec = (900-Node.e)/Node.qr;
 //    float tc  = Ec/qc;
@@ -101,15 +126,35 @@ void realCharge(WCV &Car,N &Node,float &t) {
     Car.Eall += tc * qc + tm * Car.qm;
     Car.Ereal += tc * qc * 0.9 + tm * Car.qm;
     Car.E = E - (tc * qc + tm * Car.qm);
+    Node.e = Node.emax;
 }
 
-bool needCharge(WCV& W) {
-    if (W.E < 50000 * 0.3) {
-        return true;
-    } else {
-        return false;
+
+/*
+ * 想要将更新节点状态和队列整合起来而做了尝试，但是还没投入使用因为段错误的问题仍未解决
+ */
+void GetQueue(WCV Car,N *node,std::vector<Request> servicequeue,float t,float tm)
+{
+    for(int j=0;j<sizeof(node);j++){
+        updateNode(node[j],tm);
+        if(needCharge(node[j])){
+            Request r;
+            r.x = node[j].x;
+            r.y = node[j].y;
+            r.e = node[j].e;
+            r.q = node[j].q;
+            r.t = t;
+            r.ID = node[j].ID;
+            servicequeue.push_back(r);
+
+            float dtemp = sqrt((Car.x - node[j].x) * (Car.x - node[j].x) +
+                               (Car.y - node[j].y) * (Car.y - node[j].y));
+            Car.dn = (Car.dn > dtemp) ? dtemp : Car.dn;
+            Car.df = (Car.df < dtemp) ? dtemp : Car.df;
+            Car.te = (Car.te > r.t) ? t : Car.te;
+            Car.tl = (Car.tl < r.t) ? t : Car.tl;
+        }
     }
 }
-
 
 #endif //WSN_SERVICEQUEUE_H
--- WSN/WSN/WRSN.h
+++ WSN/WSN/WRSN.h
@@ -12,7 +12,10 @@ struct WRSN{
     float E;//能耗比（充电车消耗电量比上网络接受充电电量）
     float Na;//存活比率
 };
-void initWRSN(WRSN&W,float x,float y,int n){
+/*
+ * 更新无线传感器网络，包括长、宽、节点数
+ */
+void initWRSN(WRSN &W,float x,float y,int n){
     W.X = x;
     W.Y = y;
     W.N = n;
@@ -26,16 +29,20 @@ struct N{
     float y;
 
     float e;    //residual energy of N
+    float emax;
     float q;    //consumption rate of node N
 
     float qr;   //energy receiving rate of node
     bool alive;
 };
+/*
+ * 从文件读取节点信息包括节点坐标、节点最大电量、节点耗电速率
+*/
 void initN(int n,N node[],std::string filename){
     std::ifstream infile;
     infile.open(filename);
     for(int i=0;i<n;i++){
-        infile>>node[i].x>>node[i].y>>node[i].e>>node[i].q;
+        infile>>node[i].x>>node[i].y>>node[i].e>>node[i].emax>>node[i].q;
 
         node[i].ID=i;
         node[i].qr=0.9;
@@ -45,11 +52,20 @@ void initN(int n,N node[],std::string filename){
     infile.close();
     return ;
 }
+
+/*
+ * 如果小于三成电量则需要充电
+ */
 bool needCharge(N node) {
-    if (node.e <= 270) {
+    if (node.e <= 0.3*node.emax) {
         return true;
-    } else return false;
+    } else
+        return false;
 }
+/*
+ * 更新节点状态，传入时间t、节点node
+ * 变动参数有节点是否存活、节点电量
+ */
 void updateNode(N &node,float t) {
     node.e -= t * node.q;
     if (node.e <= 0) {
